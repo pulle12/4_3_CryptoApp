551fdbc692c201ae7928be2a6c51185ce4d5a056
pulle12 <psummerauer@tsn.at>
2026-03-27 09:49:32 +0100

    feat: import more "angabe" files

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const app = Vue.createApp({
-    data() {
-        return {
-        }
-    },
-    mounted() {
-        // auto call methods after app started
+const app = new Vue({
+    el: '#app',
+    data: {
+        credentials: []
     },
     methods: {
+        hallo: function (event) {
+            alert('Hello !');
+        }
+    },
+
+    created(){
+        fetch("http://localhost/projects/passwortmanager-2/api.php?r=credentials/")
+            .then(response => response.json())
+            .then(data => (this.credentials = data))
     }
+
 })
