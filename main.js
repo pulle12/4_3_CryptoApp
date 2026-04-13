9a952804197e17295670d471658bdac365673832
piusrauch <pirauch@tsn.at>
2026-04-13 12:04:40 +0200

    main.js app component eingebaut und vue

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-        credentials: []
-    },
-    methods: {
-        hallo: function (event) {
-            alert('Hello !');
+const app = Vue.createApp({
+    data() {
+        return {
+            purchases: [],
+            wallets: [],
+            prices: {}
         }
     },
 
-    created(){
-        fetch("http://localhost/projects/passwortmanager-2/api.php?r=credentials/")
-            .then(response => response.json())
-            .then(data => (this.credentials = data))
-    }
+    created() {
+        this.loadPurchases();
+        this.loadWallets();
+        this.loadPrices();
+    },
 
-})
+    methods: {
+        loadPurchases() {
+            axios.get("http://localhost/php43_angabe/server/api/purchase")
+                .then(res => {
+                    this.purchases = res.data;
+                })
+                .catch(err => console.log(err));
+        },
+
+        loadWallets() {
+            axios.get("http://localhost/php43_angabe/server/api/wallet")
+                .then(res => {
+                    this.wallets = res.data;
+                })
+                .catch(err => console.log(err));
+        },
+
+        loadPrices() {
+            axios.get("https://api.bitpanda.com/v1/ticker")
+                .then(res => {
+                    this.prices = res.data;
+                })
+                .catch(err => console.log(err));
+        },
+
+        addPurchase(purchase) {
+            this.purchases.unshift(purchase);
+        }
+    }
+});
+
+// Komponenten registrieren
+app.component('app-display', window.AppDisplay);
+app.component('purchase-form', window.PurchaseForm);
+app.component('wallet-list', window.WalletList);
+
+// Mount
+app.mount('#app');
